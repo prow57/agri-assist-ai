55ed4dd0f579df7662321155783cfbd1a35289b1
mohamedrayen <mohamedrayen.bouzaabia@esprit.tn>
2024-07-19 18:33:42 +0100

    adding .env to .gitignore

--- main.py
+++ main.py
@@ -1,9 +1,106 @@
 # Warning control
 import warnings
 warnings.filterwarnings('ignore')
-from crewai import Agent, Task, Crew
+
+from dotenv import load_dotenv
+load_dotenv() 
 import os
+
+from crewai import Agent, Task, Crew
 from utils import get_openai_api_key
 
 openai_api_key = get_openai_api_key()
 os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
+
+
+planner = Agent(
+    role="Content Planner",
+    goal="Plan engaging and factually accurate content on {topic}",
+    backstory="You're working on planning a blog article "
+              "about the topic: {topic}."
+              "You collect information that helps the "
+              "audience learn something "
+              "and make informed decisions. "
+              "Your work is the basis for "
+              "the Content Writer to write an article on this topic.",
+    allow_delegation=False,
+	verbose=True
+)
+
+writer = Agent(
+    role="Content Writer",
+    goal="Write insightful and factually accurate "
+         "opinion piece about the topic: {topic}",
+    backstory="You're working on a writing "
+              "a new opinion piece about the topic: {topic}. "
+              "You base your writing on the work of "
+              "the Content Planner, who provides an outline "
+              "and relevant context about the topic. "
+              "You follow the main objectives and "
+              "direction of the outline, "
+              "as provide by the Content Planner. "
+              "You also provide objective and impartial insights "
+              "and back them up with information "
+              "provide by the Content Planner. "
+              "You acknowledge in your opinion piece "
+              "when your statements are opinions "
+              "as opposed to objective statements.",
+    allow_delegation=False,
+    verbose=True
+)
+
+
+editor = Agent(
+    role="Editor",
+    goal="Edit a given blog post to align with "
+         "the writing style of the organization. ",
+    backstory="You are an editor who receives a blog post "
+              "from the Content Writer. "
+              "Your goal is to review the blog post "
+              "to ensure that it follows journalistic best practices,"
+              "provides balanced viewpoints "
+              "when providing opinions or assertions, "
+              "and also avoids major controversial topics "
+              "or opinions when possible.",
+    allow_delegation=False,
+    verbose=True
+)
+
+
+write = Task(
+    description=(
+        "1. Use the content plan to craft a compelling "
+            "blog post on {topic}.\n"
+        "2. Incorporate SEO keywords naturally.\n"
+		"3. Sections/Subtitles are properly named "
+            "in an engaging manner.\n"
+        "4. Ensure the post is structured with an "
+            "engaging introduction, insightful body, "
+            "and a summarizing conclusion.\n"
+        "5. Proofread for grammatical errors and "
+            "alignment with the brand's voice.\n"
+    ),
+    expected_output="A well-written blog post "
+        "in markdown format, ready for publication, "
+        "each section should have 2 or 3 paragraphs.",
+    agent=writer,
+)
+
+
+
+edit = Task(
+    description=("Proofread the given blog post for "
+                 "grammatical errors and "
+                 "alignment with the brand's voice."),
+    expected_output="A well-written blog post in markdown format, "
+                    "ready for publication, "
+                    "each section should have 2 or 3 paragraphs.",
+    agent=editor
+)
+
+
+crew = Crew(
+    agents=[planner, writer, editor],
+    tasks=[plan, write, edit],
+    verbose=2
+)
