094f894d6b453835ca657d0a54d2a2aeacd554f8
prow57naff <bed-com-08-19@unima.ac.mw>
2024-07-19 17:21:38 +0200

    confgured firestore firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// firebase.js
+const admin = require('firebase-admin');
+const serviceAccount = require('./path/to/your/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://<your-database-name>.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+module.exports = db;
