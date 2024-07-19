2aecffb2600c29efc8790bbb8aa456ef6433bf31
ChrisProg97 <chrispinjossam@gmail.com>
2024-07-20 00:57:38 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/agri/MainActivity.kt
+++ app/src/main/java/com/example/agri/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.agri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the button by its ID
+        val scanLeafButton: Button = findViewById(R.id.scan)
+
+        // Set an OnClickListener on the button
+        scanLeafButton.setOnClickListener {
+            // Create an Intent to start the ScanLeafActivity
+            val intent = Intent(this, ScanLeafActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
