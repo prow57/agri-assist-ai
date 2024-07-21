434df5c75ca7880f537ad79dc39f1c97c5e510f1
immanuelbester <immanuelbester10@gmail.com>
2024-07-21 13:43:56 +0200

    updated firebase config

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./agriassist.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://agriassist-ai-b6fa4-default-rtdb.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+module.exports = db;
