5e33b28d5df3846854256f98a87107f56fb6daa6
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-05 14:03:09 +0000

    fixed healthy assessment error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const healthRoutes = require('./routes/healthRoutes');
 require('dotenv').config();
 
 const app = express();
