e97bdd60a8b345c6ccfdc3968d3b8aefb01cc89f
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-08-06 17:19:26 +0200

    Updated by adding detail page route

--- agrifrontend/lib/homepages/weather/ui/weather_screen.dart
+++ agrifrontend/lib/homepages/weather/ui/weather_screen.dart
@@ -1,6 +1,8 @@
 // weather_page.dart
 import 'package:agrifrontend/homepages/weather/weather_forecasting.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:agrifrontend/homepages/weather/ui/detail_page.dart';
 
 class WeatherPage extends StatefulWidget {
   @override
@@ -64,26 +66,70 @@ class _WeatherPageState extends State<WeatherPage> {
       return [Text('No forecast data available')];
     }
 
-    return List<Widget>.generate(7, (index) {
+  return List<Widget>.generate(7, (index) {
       final dayForecast = _weatherData!['forecast']['forecastday'][index];
-      return Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.network('https:${dayForecast['day']['condition']['icon']}'),
-              Text('Date: ${dayForecast['date']}'),
-              Text('Temp: ${dayForecast['day']['avgtemp_c']}°C'),
-              Text('Condition: ${dayForecast['day']['condition']['text']}'),
-            ],
+  
+      // Convert the date string to a DateTime object
+      final date = DateTime.parse(dayForecast['date']);
+  
+      // Format the date to display the day of the week
+      final dayOfWeek = DateFormat('EEEE').format(date);
+
+      return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailPage(dayForecast: dayForecast),
+            ),
+          );
+        },
+        child: SizedBox(
+          child: Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    dayOfWeek,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Image.network(
+                    'https:${dayForecast['day']['condition']['icon']}',
+                    height: 100,
+                    width: 100,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '${dayForecast['day']['avgtemp_c']}°C',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '${dayForecast['day']['condition']['text']}',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       );
     });
-  }
+
+}
 
   String _getRecommendation(String condition) {
     if (condition.contains('rain')) {
@@ -97,6 +143,8 @@ class _WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('EEEE, h:mm a').format(now);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -129,15 +177,20 @@ class _WeatherPageState extends State<WeatherPage> {
                     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    'Mon, September 16 14:15',
+                    // 'Mon, September 16 14:15',
+                    formattedDate,
                     style: TextStyle(fontSize: 16, color: Colors.grey),
                   ),
                   SizedBox(height: 20),
                   Row(
                     children: [
-                      Image.network(
-                        'https:${_weatherData!['current']['condition']['icon']}',
-                        width: 50,
+                      SizedBox(
+                        height: 150,
+                        width: 150, 
+                        child: Image.network(
+                          'https:${_weatherData!['current']['condition']['icon']}',
+                          fit: BoxFit.cover,
+                        ),
                       ),
                       SizedBox(width: 20),
                       Column(
