ba945024718357e99826d6cd65f0d6bd638837fb
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-08-13 10:37:51 +0200

    Create authMiddleware.js

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+// middlewares/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authMiddleware;
