da7e79bbb513ce77cd261feb3d09e99f779f6f7c
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-08-13 10:38:59 +0200

    Updated for crop price functionality

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
+require('dotenv').config();
+const db = require('./db');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const healthRoutes = require('./routes/healthRoutes');
-require('dotenv').config();
+const cropPriceRoutes = require('./routes/cropPriceRoutes');
+const userRoutes = require('./routes/userRoutes'); 
 
 const app = express();
 
+// Test the connection
+db.connect()
+  .then(obj => {
+    obj.done(); // success, release the connection;
+    console.log("PostgreSQL connection successful");
+  })
+  .catch(error => {
+    console.log("Error connecting to PostgreSQL:", error);
+  });
+
+// Middleware setup
 app.use(bodyParser.json());
-app.use(express.json);
+app.use(express.json());
+
+// Route setup
 app.use('/api', routes);
 app.use('/api/health', healthRoutes);
+app.use('/api/crop-prices', cropPriceRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
