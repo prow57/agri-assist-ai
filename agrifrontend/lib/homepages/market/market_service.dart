47c9c14b9edd9f873e40a8eb6fa993b61fd80eb1
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-08-16 14:25:15 +0200

    Update market_service.dart

--- agrifrontend/lib/homepages/market/market_service.dart
+++ agrifrontend/lib/homepages/market/market_service.dart
@@ -1,34 +1,31 @@
+// market_service.dart
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'market.dart'; // Import your Market model
+import 'commodity.dart'; // Import your Commodity model
 
 class MarketService {
-  static const String apiUrl =
-      'https://www.nsomalawi.mw/publications/agriculture/monthly-statistical-bulletin-2024'; // Replace with the actual endpoint
+  final String baseUrl = 'http://localhost:3000/api';
 
-  Future<List<Commodity>> fetchMarketPrices() async {
-    final response = await http.get(Uri.parse(apiUrl));
+  Future<List<Market>> fetchMarkets() async {
+    final response = await http.get(Uri.parse('$baseUrl/markets'));
 
     if (response.statusCode == 200) {
-      List<dynamic> data = json.decode(
-          response.body); // Adjust according to the actual JSON structure
+      List<dynamic> data = json.decode(response.body);
+      return data.map((item) => Market.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load markets');
+    }
+  }
+
+  Future<List<Commodity>> fetchMarketPrices(String marketId) async {
+    final response = await http.get(Uri.parse('$baseUrl/prices/market/$marketId'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
       return data.map((item) => Commodity.fromJson(item)).toList();
     } else {
       throw Exception('Failed to load market prices');
     }
-  }
-}
-
-class Commodity {
-  final String name;
-  final double supply;
-
-  Commodity({required this.name, required this.supply});
-
-  factory Commodity.fromJson(Map<String, dynamic> json) {
-    return Commodity(
-      name: json['product_name'], // Adjust according to actual JSON structure
-      supply: json['supply']
-          .toDouble(), // Adjust according to actual JSON structure
-    );
   }
 }
