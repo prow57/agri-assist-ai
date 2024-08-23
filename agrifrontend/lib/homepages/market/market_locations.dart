7b13a3d8978254503d67c9ed9e7a6f4e9774547a
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-23 13:02:46 +0200

    Updated map for market_locations.dart

--- agrifrontend/lib/homepages/market/market_locations.dart
+++ agrifrontend/lib/homepages/market/market_locations.dart
@@ -2,23 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-class MarketLocationPage extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Malawi Market Locations'),
+        title: Text('Map of Malawi'),
       ),
       body: FlutterMap(
         options: MapOptions(
-          center: LatLng(-13.2543, 34.3015),
-          zoom: 7.0,
+          center: LatLng(-13.2543, 34.3015), // Center on Malawi
+          zoom: 6.0, // Set zoom level
         ),
         layers: [
           TileLayerOptions(
-            urlTemplate: "https://api.mapbox.com/styles/v1/prow/clh9jmy6d001501qe6i8us3ep/tiles/256/{z}/{x}/{y}@2x?access_token={access_token}",
+            urlTemplate:
+                "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHJvdyIsImEiOiJjbTA2bGtwdDgwdHlmMmlzMjdzYnRvY250In0.Zk6PlpPy_A4lcceroLJM4g",
             additionalOptions: {
-              'access_token': 'pk.eyJ1IjoicHJvdyIsImEiOiJjbTA1MTFwOGIwZzVwMmlzZDUzNzh2YnRxIn0.lXsl2BkrTYO168oBeXUZgQ',
+              'accessToken':
+                  'pk.eyJ1IjoicHJvdyIsImEiOiJjbTA2bGtwdDgwdHlmMmlzMjdzYnRvY250In0.Zk6PlpPy_A4lcceroLJM4g',
+              'id': 'mapbox.mapbox-streets-v7'
             },
           ),
           MarkerLayerOptions(
@@ -27,8 +46,18 @@ class MarketLocationPage extends StatelessWidget {
                 width: 80.0,
                 height: 80.0,
                 point: LatLng(-13.9626, 33.7741),
-                builder: (ctx) => const Icon(
-                  Icons.location_on,
+                builder: (ctx) => Icon(
+                  Icons.location_pin,
+                  color: Colors.red,
+                  size: 40.0,
+                ),
+              ),
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: LatLng(-15.7861, 35.0058),
+                builder: (ctx) => Icon(
+                  Icons.location_pin,
                   color: Colors.red,
                   size: 40.0,
                 ),
@@ -39,6 +68,4 @@ class MarketLocationPage extends StatelessWidget {
       ),
     );
   }
-
-
 }
