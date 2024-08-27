3c7b120d4d3708d2b4ca18c995dc268bdaa81d01
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-27 09:27:35 +0200

    Update index.js

---
+++ agriback/index.js
@@ -0,0 +1,41 @@
+// index.js
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); // Load environment variables
+
+// Import the db and storage instances from db.js
+const { db } = require('./db');
+
+// Import route handlers
+const courseRoutes = require('./src/routes/courseRoutes');
+const adviceRoutes = require('./src/routes/adviceRoutes');
+const chatRoutes = require('./src/routes/chatRoutes');
+const authRoutes = require('./src/routes/authRoutes');
+const otpRoutes = require('./src/routes/otpRoutes');
+const cropRoutes = require('./src/routes/cropRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes for Endpoints 
+app.use('/api/verify', otpRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/advice', adviceRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/vision', cropRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Agri-Assist-AI Backend API is running');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
