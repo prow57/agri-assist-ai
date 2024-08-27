35ebd3f53c3ef226bf427d35a533ed5365c06826
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-08-27 09:35:28 +0200

    Update db.js

---
+++ agriback/db.js
@@ -0,0 +1,16 @@
+// Initialize Firebase Admin SDK
+const admin = require('firebase-admin');
+require('dotenv').config(); // Load environment variables early
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'gs://agriassist-a7e77.appspot.com',
+});
+
+const bucket = admin.storage().bucket();
+const db = admin.firestore(); // Initialize Firestore
+
+// Export db so it can be used in other modules
+module.exports = { db,bucket };
