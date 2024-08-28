deeced2be5b059a47a0dc7af386cb7a1d8cce386
ChrisProg97 <chrispinjossam@gmail.com>
2024-08-28 10:10:32 +0200

    imported some dependencies

--- agrifrontend/lib/authentication/signin.dart
+++ agrifrontend/lib/authentication/signin.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'package:agrifrontend/authentication/mobilenumber.dart';
 import 'package:agrifrontend/authentication/signup.dart';
+import 'package:agrifrontend/personalization/community.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:agrifrontend/preferences/community.dart'; // Import the Community page
 
 class Signin extends StatefulWidget {
   @override
