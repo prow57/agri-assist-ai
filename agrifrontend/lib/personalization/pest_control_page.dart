cdb07a81761673d2669ff24946403d236b803c24
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-09-11 14:04:48 +0200

    Update pest_control_page.dart

--- agrifrontend/lib/personalization/pest_control_page.dart
+++ agrifrontend/lib/personalization/pest_control_page.dart
@@ -1,23 +1,4 @@
-import 'package:flutter/material.dart';
 
-class PestControlPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Pest Control')),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            'Effective methods for controlling pests include using biological pest control, chemical treatments, '
-            'and integrated pest management techniques.',
-            style: TextStyle(fontSize: 16),
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
