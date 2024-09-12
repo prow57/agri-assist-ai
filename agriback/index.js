eb4032596d4714b2f8f0a15523d8b516eba1379f
Emmanuel Bester <95228455+prow57@users.noreply.github.com>
2024-09-12 07:54:07 +0200

    Update index.js

--- agriback/index.js
+++ agriback/index.js
@@ -8,12 +8,14 @@ require('dotenv').config(); // Load environment variables
 const { db } = require('./db');
 
 // Import route handlers
+const recommendRoutes = require('./src/routes/recommendRoutes');
 const courseRoutes = require('./src/routes/courseRoutes');
 const adviceRoutes = require('./src/routes/adviceRoutes');
 const chatRoutes = require('./src/routes/chatRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 const otpRoutes = require('./src/routes/otpRoutes');
 const cropRoutes = require('./src/routes/cropRoutes');
+const communityRoute = require('./src/routes/communityRoute');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -29,6 +31,8 @@ app.use('/api/courses', courseRoutes);
 app.use('/api/advice', adviceRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/vision', cropRoutes);
+app.use('/api/community', communityRoute);
+app.use('/api/recommend', recommendRoutes);
 
 // Default route
 app.get('/', (req, res) => {
